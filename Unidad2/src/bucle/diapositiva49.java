f610691619b1aed31565c6a483ddee037864ea26
nachodesig <doritaaarubiii@gmail.com>
2025-11-11 14:42:11 +0100

    Create diapositiva49.java

---
+++ Unidad2/src/bucle/diapositiva49.java
@@ -0,0 +1,22 @@
+package bucle;
+
+import java.util.Scanner;
+
+public class diapositiva49 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce el número de veces que quieres hacer la pregunta: ");
+		int numusuario = sc.nextInt();
+		for(int contador = 0; contador != numusuario ;contador ++) {
+			
+			System.out.println("hoy vemos pelicula,vladi");
+			
+		}
+		
+	}
+
+}
